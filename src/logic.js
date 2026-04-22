a79083eda9bd6c3d59a5dabca67258aa6641ee2a
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-04-22 16:37:06 +0530

    initial todo logic

--- src/logic.js
+++ src/logic.js
@@ -1,3 +1,28 @@
 class todoMain {
-    
-};
+  constructor(todoTitle, startDate, endDate, todoDescription, priority) {
+    this.createTime = Date.now();
+    this.todoTitle = todoTitle;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.todoDescription = todoDescription;
+    this.priority = priority;
+    this.todoComplete = false;
+  }
+  toggleStatus() {
+    this.todoComplete = !this.todoComplete;
+  }
+}
+
+class Project {
+  constructor(projectName) {
+    this.projectTasks = [];
+    this.projectName = projectName;
+  }
+}
+
+function addTask() {
+  todoLists.push(projectTasks);
+  console.log(todoLists);
+}
+
+addTask("Buy Keyboard");
