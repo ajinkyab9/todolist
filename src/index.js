fa78ef70f3ff4e3a870936fe22cec8ff029989ec
Ajinkya <87272959+ajinkyab9@users.noreply.github.com>
2026-05-07 16:59:02 +0530

    added editing feature

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { triggerNewTodo, refreshUI } from "./ui_handler.js";
 import { saveToLocalStorage, loadFromLocalStorage } from "./state_manager.js";
 import "./styles.css";
 
+
 // const newProject = new Project();
 const newProject = loadFromLocalStorage();
 const todoForm = document.getElementById("todoForm");
@@ -12,14 +13,30 @@ refreshUI(newProject.projectTasks);
 
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  const editingId = todoForm.dataset.editingId;
+
   const title = document.getElementById("todoTitle").value;
   const desc = document.getElementById("todoDescription").value;
   const due = document.getElementById("dueDate").value;
   const priority = document.getElementById("selectPriority").value;
   const notes = document.getElementById("todoNotes").value;
 
-  const newTask = new todoMain(title, due, desc, priority, notes);
-  newProject.addTask(newTask);
+  if (editingId) {
+    const taskToUpdate = newProject.getTaskId(editingId);
+
+    if (taskToUpdate) {
+      taskToUpdate.todoTitle = title;
+      taskToUpdate.todoDescription = desc;
+      taskToUpdate.dueDate = due;
+      taskToUpdate.priority = priority;
+      taskToUpdate.todoNotes = notes;
+    }
+    delete todoForm.dataset.editingId;
+  } else {
+    const newTask = new todoMain(title, due, desc, priority, notes);
+    newProject.addTask(newTask);
+  }
 
   refreshUI(newProject.projectTasks);
   saveToLocalStorage(newProject);
@@ -36,8 +53,23 @@ listContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("deleteTaskBtn")) {
     newProject.deleteTask(taskId);
     refreshUI(newProject.projectTasks);
-
     saveToLocalStorage(newProject);
+  }
+
+  if (e.target.classList.contains("editTaskBtn")) {
+    const taskToEdit = newProject.getTaskId(taskId);
+    document.getElementById("todoTitle").value = taskToEdit.todoTitle;
+    document.getElementById("todoDescription").value =
+      taskToEdit.todoDescription;
+    document.getElementById("dueDate").value = taskToEdit.dueDate;
+    document.getElementById("selectPriority").value = taskToEdit.priority;
+    document.getElementById("todoNotes").value = taskToEdit.todoNotes || "";
+
+    /**NOTE: to ensure that task knows the todo is being edited */
+    todoForm.dataset.editingId = taskId;
+
+    /**Open the modal for editing */
+    triggerNewTodo.showModal();
   }
 
   if (e.target.classList.contains("todoCheck")) {
